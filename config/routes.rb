5973b95b6b0c28177176ef16857157da0652d360
Yoni Doron-Peters <yonidoronpeters@gmail.com>
2013-11-18 14:48:03 -0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 WingMe::Application.routes.draw do
-  
-  get "users/new"
+  resources :users
   root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
